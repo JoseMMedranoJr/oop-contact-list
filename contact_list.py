fc32197607392ee03329308b4f4728708c9b608e
JoseMMedranoJr <jose.m.medranojr+port@gmail.com>
2025-11-29 14:19:15 -0600

    Commit all modified and new files

--- contact_list.py
+++ contact_list.py
@@ -1,2 +1,25 @@
 class ContactList:
+    classmates = {}
 
+    def __init__(self, name, number):
+        self.name = name 
+        self.number = number
+ 
+    @classmethod
+    def add_contact(cls, name, number):
+        ContactList.classmates[name] = number 
+    
+    @classmethod
+    def remove_contact(self, name):
+        if name in the list, then remove from list
+        
+        return(self)
+
+    def find_shared_contacts(self, name):
+        return(self)
+
+dude1 = ContactList.add_contact('Brian', 123456)
+dude2 = ContactList.add_contact('Dan', 9876543)
+
+print(ContactList.classmates)
+#something = classmates.add_contact
